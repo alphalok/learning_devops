33574bfa36919344ccad6b96137c8e352055aa25
alphalok <eelhousni3@gmail.com>
2024-01-04 00:55:39 +0100

    configure version increment in build stage

--- script.groovy
+++ script.groovy
@@ -1,14 +1,14 @@
 def buildJar(){
     echo "building the application ..."
-    sh 'mvn package'
+    sh 'mvn clean package'
 }
 
 def buildImage(){
     echo "building the docker image ..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER' )]){
-        sh 'docker build -t okooel/learning:jma-2.0 .'
+        sh "docker build -t okooel/learning:${IMAGE_NAME} ."
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh'docker push okooel/learning:jma-2.0'
+        sh "docker push okooel/learning:${IMAGE_NAME}"
 
     }
 }
